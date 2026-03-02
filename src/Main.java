0ec995eba828b3cacd1ca7ffac77d0218828e8e3
Fris233 <baefris@gmail.com>
2026-03-02 22:58:18 +0200

    code maktoob 3la seyam

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+/*
+o username
+o password  :validation يعملوا .
+        2
+o username  مش فاضي
+o password >= 8  لو البيانات غلط  Exception يعملوا .
+3 : يستخدموا .
+4
+o Conditions
+o Methods                      Joudy
+o Exception Handling           Faris
+o ArrayList لتخزين users       Omar
+*/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        /*
+        */
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
