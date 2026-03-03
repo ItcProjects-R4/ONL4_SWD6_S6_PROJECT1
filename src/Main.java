868883228b8b55683208544a4be6f14d72648f94
Fris233 <baefris@gmail.com>
2026-03-03 17:33:23 +0200

    Added exception handling based on requirements

--- src/Main.java
+++ src/Main.java
@@ -20,35 +20,75 @@ public class Main {
     static ArrayList<User> users = new ArrayList<>();
 
     // Joudy Work
-    public static String Username(String name){
-
+    public static String Username(){
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a valid username.");
-        name = input.nextLine();
+        String user = input.nextLine();
 
-        return name;
+        if(user.trim().equals("")){
+            throw new IllegalArgumentException("Username cannot be empty.");
+        }
+        else {
+            for(User u : users){
+                if(u.getName().equals(user)){
+                    throw new IllegalArgumentException("Username already exists.");
+                }
+            }
+        }
+        return user;
     }
 
-    public static String Password(String pass){
+    public static void Password(String name){
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a valid password.");
-        pass = input.nextLine();
+        String password = input.nextLine().trim();
+        if(password.trim().equals("") || password.length() < 8){
+            throw new IllegalArgumentException("Password cannot be empty and be more than 8 characters.");
+        }
+        for(User u : users){
+            if(u.getName().equals(name) && u.getPassword().equals(password)){
+                System.out.println("Successfully logged in with an existing account.");
+            } else {
+               users.add(new User(name, password));
+                System.out.println("Successfully logged in with a new account.");
+            }
+        }
 
-        return pass;
     }
 
     public static void main(String[] args) {
 
         users.add(new User("admin","12345678"));
 
-        String name="";
+        Scanner input = new Scanner(System.in);
+        String currentName="";
         String pass="";
 
-        name = Username(name);
-        pass = Password(pass);
+        System.out.println("");
+        while(true){
+            System.out.println("Press enter to start:");
+            if(input.nextLine().equals("PRINTALLUSERS")){
+                for(User u : users){
+                    System.out.println(u);
+                }
+            }
+            try {
+                 currentName = Username();
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage() + " Try again.");
+                continue;
+            }
+            try{
+                Password(currentName);
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage() + " \nTry again.");
+                continue;
+            }
+        }
 
     }
 }
